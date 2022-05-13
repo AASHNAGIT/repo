7150dc89f7a09cc34435471f90ed3027f3fa6bc2
AASHNAGIT <githubaash@gmail.com>
2022-05-13 11:00:59 +0530

    first commit

---
+++ BootAuthorizationServer/src/test/java/com/example/authorizationserver/BootAuthorizationServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.authorizationserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
